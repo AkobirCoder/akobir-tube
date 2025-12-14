f099dd808203f9b6e98480ffdb9673bedffa5e23
AkobirCoder <akobir0308@gmail.com>
2025-12-14 20:03:15 +0500

    ./src/components/app/App.jsx was updated

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import {Stack, Button, Container, Box} from '@mui/material';
+import {Container, Box} from '@mui/material';
 
 import {Main, Channel} from '../index';
 
 const App = () => {
     return (
         <Container>
-            <Stack spacing={2} direction={'row'}>
+            {/* <Stack spacing={2} direction={'row'}>
                 <Button variant='text'>Text</Button>
                 <Button variant='contained'>Contained</Button>
                 <Button variant='outlined'>Outlined</Button>
-            </Stack>
+            </Stack> */}
             <Box>
                 <Routes>
                     <Route path='/' element={<Main />} />
