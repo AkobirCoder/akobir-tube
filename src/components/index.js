eca4bd35f317c3d7bf393914aea63adc531a4067
AkobirCoder <akobir0308@gmail.com>
2025-12-16 00:09:29 +0500

    ./src/components/index.js was updated

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export {default as Main} from './main/Main';
 export {default as Navbar} from './navbar/Navbar';
 export {default as SearchBar} from './search-bar/Search-bar';
 export {default as Profile} from './profile/Profile';
+export {default as Category} from './category/Category';
 export {default as VideoDetail} from './video-detail/Video-Detail';
 export {default as Channel} from './channel/Channel';
 export {default as Search} from './search/Search';
