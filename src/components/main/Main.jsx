3f54838389dfe4dd47a67bb8ea6500bcdab4acd0
AkobirCoder <akobir0308@gmail.com>
2025-12-16 00:09:43 +0500

    ./src/components/main/Main.jsx was updated

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Stack, Container, Box, Typography} from '@mui/material';
 
+import {colors} from '../../constants/colors';
+import {Category} from '../index';
+
 const Main = () => {
+    const [selectedCategory, setSelectedCategory] = useState('New');
+
     return (
         <Stack>
-            Category
+            <Category />
             <Box p={2} sx={{height: '90vh'}}>
                 <Container maxWidth={'90%'}>
                     <Typography variant={'h4'} fontWeight={'bold'} mb={2}>
-
+                        {selectedCategory} <span style={{color: colors.secondary}}>
+                            videos
+                        </span>
                     </Typography>
+                    Videos
                 </Container>
             </Box>
         </Stack>
