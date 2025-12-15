da8214e4debcb15baa5eca10d827e83950be0130
AkobirCoder <akobir0308@gmail.com>
2025-12-16 00:34:12 +0500

    ./src/components/category/Category.jsx was added

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Stack } from '@mui/material';
+
+import {category} from '../../constants/category';
+import {colors} from '../../constants/colors';
 
 const Category = () => {
     return (
-        <div>Category</div>
+        <Stack direction={'row'} sx={{overflowX: 'scroll'}}>
+            {
+                category.map((item) => {
+                    return (
+                        <button 
+                            key={item.name}
+                            className='category-btn'
+                            style={{
+                                borderRadius: 0,
+
+                            }}
+                        >
+                            <span style={{color: colors.secondary, marginRight: '15px'}}>
+                                {item.icon}
+                            </span>
+                            <span style={{opacity: 1}}>
+                                {item.name}
+                            </span>
+                    </button>
+                    );
+                })
+            }
+        </Stack>
     );
 }
 
