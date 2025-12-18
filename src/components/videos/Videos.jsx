6fb6e847311c9f1147b638b93deb2e11a2a5a127
AkobirCoder <akobir0308@gmail.com>
2025-12-18 13:27:14 +0500

    ./src/components/videos/Videos.jsx was updated

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Stack, Box } from '@mui/material';
 
-const Videos = () => {
+import {VideoCard} from '../index'
+
+const Videos = ({videos}) => {
+    console.log(videos);
+
     return (
-        <div>Videos</div>
+        <Stack 
+            width={'100%'} 
+            direction={'row'} 
+            flexWrap={'wrap'}
+            justifyContent={'start'}
+            alignItems={'center'}
+            gap={2}
+        >  
+            {
+                videos.map((item) => {
+                    return (
+                        <Box key={item.id.videoId}>
+                            {
+                                item.id.videoId && <VideoCard video={item} />
+                            }
+                        </Box>
+                    );
+                })
+            }
+        </Stack>
     );
 }
 
