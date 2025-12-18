646803fb23d56df949028d6ced6a148d407836fa
AkobirCoder <akobir0308@gmail.com>
2025-12-18 23:41:00 +0500

    ./src/components/search-bar/Search-bar.jsx was updated

--- src/components/search-bar/Search-bar.jsx
+++ src/components/search-bar/Search-bar.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
-import {colors} from '../../constants/colors'
+import {colors} from '../../constants/colors';
  
 const SearchBar = () => {
+    const [value, setValue] = useState('');
+    const navigate = useNavigate();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        console.log(value);
+
+        if (value) {
+            navigate(`/search/${value}`);
+        }
+
+        setValue('');
+    }
+
     return (
         <Paper 
-            component={'form'} 
+            component={'form'}
+            onSubmit={submitHandler}
             sx={{
                 border: `1px solid ${colors.secondary}`,
                 pl: 2,
                 boxShadow: 'none',
-                borderRadius: 0,
                 mr: 5
             }}
         >
-            <input type="text" placeholder='Search...' className='search-bar' />
-            <IconButton>
+            <input 
+                type="text" 
+                placeholder='Search...' 
+                className='search-bar' 
+                value={value} 
+                onChange={(event) => setValue(event.target.value)} 
+            />
+            <IconButton type='submit'>
                 <Search />
             </IconButton>
         </Paper>
