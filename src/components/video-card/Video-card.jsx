fddc9c63c0c13d5abd90929e09b4c16adeef618a
AkobirCoder <akobir0308@gmail.com>
2025-12-18 13:27:30 +0500

    ./src/components/video-card/Video-card.jsx was added

---
+++ src/components/video-card/Video-card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Card, CardMedia} from '@mui/material';
+
+const VideoCard = ({video}) => {
+    console.log(video);
+
+    return (
+        <Card 
+            sx={{
+                width: '320px',
+                boxShadow: 'none',
+                borderRadius: 0
+            }}
+        >
+            <CardMedia image={video?.snippet?.thumbnails?.high?.url} />
+        </Card>
+    );
+}
+
+export default VideoCard;
