8473b8f44087b2db32f81e81902ea288462eb52f
AkobirCoder <akobir0308@gmail.com>
2025-12-18 14:30:01 +0500

    ./src/components/video-card/Video-card.jsx was updated

--- src/components/video-card/Video-card.jsx
+++ src/components/video-card/Video-card.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
-import {Card, CardMedia} from '@mui/material';
+import React, { Fragment } from 'react';
+import {Avatar, Card, CardContent, CardMedia, Stack, Typography} from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import moment from 'moment/moment';
+
+import {colors} from '../../constants/colors';
 
 const VideoCard = ({video}) => {
     console.log(video);
@@ -12,7 +16,48 @@ const VideoCard = ({video}) => {
                 borderRadius: 0
             }}
         >
-            <CardMedia image={video?.snippet?.thumbnails?.high?.url} />
+            <CardMedia 
+                image={video?.snippet?.thumbnails?.high?.url} 
+                alt={video?.snippet?.title}
+                sx={{
+                    width: '360px',
+                    height: '180px'
+                }} 
+            />
+            <CardContent 
+                sx={{
+                    background: colors.primary,
+                    height: '200px',
+                    position: 'relative'
+                }}
+            >
+                <Fragment>
+                    <Typography my={'5px'} sx={{opacity: '0.4'}}>
+                        {moment(video?.snippet?.publishedAt).fromNow()}
+                    </Typography>
+                    <Typography variant='subtitle1' fontWeight={'bold'}>
+                        {video?.snippet?.title.slice(0, 50)}
+                    </Typography>
+                    <Typography variant='subtitle2' sx={{opacity: '0.6'}}>
+                        {video?.snippet?.description.slice(0, 70)}
+                    </Typography>
+                </Fragment>
+                <Fragment>
+                    <Stack 
+                        direction={'row'} 
+                        position={'absolute'}
+                        bottom={'10px'}
+                        alignItems={'center'}
+                        gap={'5px'}
+                    >
+                        <Avatar src={video?.snippet?.thumbnails?.high?.url} />
+                        <Typography variant='subtitle2' color='gray'>
+                            {video?.snippet?.channelTitle}
+                            <CheckCircle sx={{fontSize: '12px', color: 'gray', ml: '5px'}} />
+                        </Typography>
+                    </Stack>
+                </Fragment>
+            </CardContent>
         </Card>
     );
 }
