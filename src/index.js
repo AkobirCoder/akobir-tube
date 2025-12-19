239896b5eaf50f4cb48cb6212d3aeb932c93710e
AkobirCoder <akobir0308@gmail.com>
2025-12-20 00:16:20 +0500

    ./src/index.js was updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import '@fontsource/roboto/700.css';
 
 import { App } from './components';
 
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
