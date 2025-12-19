6f540290b0a5a8ff5e82b40fec476f018a6acfcb
AkobirCoder <akobir0308@gmail.com>
2025-12-19 22:35:52 +0500

    ./src/components/videos/Videos.jsx was updated

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack, Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 
 import {VideoCard, ChannelCard} from '../index'
 
@@ -7,29 +7,27 @@ const Videos = ({videos}) => {
     console.log(videos);
 
     return (
-        <Stack 
-            width={'100%'} 
-            direction={'row'} 
-            flexWrap={'wrap'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            gap={2}
-        >  
-            {
-                videos.map((item) => {
-                    return (
-                        <Box key={item.id.videoId}>
-                            {
-                                item.id.videoId && <VideoCard video={item} />
-                            }
-                            {
-                                item.id.channelId && <ChannelCard video={item} />
-                            }
-                        </Box>
-                    );
-                })
-            }
-        </Stack>
+        <Box sx={{flexGrow: 1}}>
+            <Grid container spacing={2}
+                justifyContent={'center'}
+                alignItems={'center'}
+            >
+                {
+                    videos.map((item) => {
+                        return (
+                            <Grid size={{xs: 12, sm: 6, md: 4, lg: 3}} key={item.id.videoId}>
+                                {
+                                    item.id.videoId && <VideoCard video={item} />
+                                }
+                                {
+                                    item.id.channelId && <ChannelCard video={item} />
+                                }
+                            </Grid>
+                        );
+                    })
+                }
+            </Grid>
+        </Box>
     );
 }
 
